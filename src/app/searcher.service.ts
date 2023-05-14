b023fb293edcbf6266d1c8e16851112b43da0c21
Tn-ko <daniiltarasov146@gmail.com>
2023-05-14 14:16:24 +0300

    added imageService

--- src/app/searcher.service.ts
+++ src/app/searcher.service.ts
@@ -1,27 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environments';
+import { environment } from 'src/environments/environment';
 import { Searcher } from './searcher';
 
-@Injectable({providedIn: 'root' })//'root' : The application-level injector in most apps. Allows app know about the service
+@Injectable({ providedIn: 'root' })//'root' : The application-level injector in most apps. Allows app know about the service
 export class SearcherService {
-  private apiServerUrl = environment.baseApiUrl;
+  private apiServerUrl = environment.baseApiURL;
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient){}
 
   public getSearchers(): Observable<Searcher[]> {
     return this.http.get<Searcher[]>(`${this.apiServerUrl}/searcher/all`);
   }
-     
+
   public addSearcher(searcher: Searcher): Observable<Searcher> {
     return this.http.post<Searcher>(`${this.apiServerUrl}/searcher/add`, searcher);
   }
 
   public updateSearcher(searcher: Searcher): Observable<Searcher> {
-    return this.http.put<Searcher>(`${this.apiServerUrl}/searcher/update`, searcher);    
+    return this.http.put<Searcher>(`${this.apiServerUrl}/searcher/update`, searcher);
   }
   public deleteSearcher(searcherId: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiServerUrl}/searcher/delete/${searcherId}`);    
+    return this.http.delete<void>(`${this.apiServerUrl}/searcher/delete/${searcherId}`);
   }
 }
+
